16b86023abe9fd253fbdcee91da66c73abbd82c8
vicky-voin <alteneria127@gmail.com>
2025-07-28 13:55:08 -0400

    Added a way to add a new book

--- app.js
+++ app.js
@@ -6,6 +6,39 @@ addBookToLibrary("Fyodor Dostoyevsky", "Cime and Punishment", true);
 addBookToLibrary("Leo Tolstoy", "War and Peace", false);
 displayBooks();
 
+const newBookForm = document.getElementById("add-book");
+const addBookBtn = newBookForm.querySelector("#submitBtn");
+const authorInput = newBookForm.querySelector("#book-author");
+const titleInput = newBookForm.querySelector("#book-title");
+const statusInput = newBookForm.querySelector("#book-status");
+
+newBookForm.addEventListener("close", (e) =>
+{
+    if(newBookForm.returnValue !== "default" &&
+    newBookForm.returnValue !== "cancel" )
+    {
+        let newBook = addBookToLibrary(
+            authorInput.value,
+            titleInput.value,
+            statusInput.checked && statusInput.value === 'on'
+        );
+
+        displayBook(newBook);
+    }
+});
+
+addBookBtn.addEventListener("click", (event) =>
+{
+    event.preventDefault();
+    newBookForm.close("added");
+})
+
+const addNewBtn = document.querySelector("button.new-book");
+addNewBtn.addEventListener("click", () =>
+{
+    displayDialog();
+});
+
 function Book(author, title, wasRead)
 {
     if(!new.target)
@@ -23,20 +56,33 @@ function addBookToLibrary(author, title, wasRead)
 {
     let book = new Book(author, title, wasRead);
     myLibrary.push(book);
+
+    return book;
 }
 
 function displayBooks()
 {
+    myLibrary.forEach(book => {
+        displayBook(book);
+
+    });
+}
+
+function displayBook(book){
+
     const template = document.querySelector("#book-card");
     const booksContainer = document.querySelector(".books");
 
-    myLibrary.forEach(book => {
-        const card = template.content.cloneNode(true);
+    const card = template.content.cloneNode(true);
 
-        card.querySelector(".card-title").textContent = book.title;
-        card.querySelector(".card-subtitle").textContent = book.author;
-        card.querySelector(".card-status").checked = book.wasRead;
+    card.querySelector(".card-title").textContent = book.title;
+    card.querySelector(".card-subtitle").textContent = book.author;
+    card.querySelector(".card-status").checked = book.wasRead;
 
-        booksContainer.appendChild(card);
-    });
+    booksContainer.appendChild(card);
+}
+
+function displayDialog()
+{
+    newBookForm.showModal();
 }
